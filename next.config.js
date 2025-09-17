82b0adf3d46b9d40bf0cb6dbf7a83a86eff2a013
Saymandev <devrabbi255@gmail.com>
2025-09-18 00:48:16 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'css')],
+  },
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+};
+
+module.exports = nextConfig;
